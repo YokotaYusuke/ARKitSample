5c5e961b00addd0d2d1a5fbfc0472ed7550a36ba
YokotaYusuke <tarut8166@gmail.com>
2024-10-18 01:02:12 +0900

    3Dモデルをジェスチャー操作できるようにした

--- ARKitSample/ContentView.swift
+++ ARKitSample/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import UIKit
 
 struct ContentView : View {
     var body: some View {
@@ -10,32 +11,53 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
         
         let config = ARWorldTrackingConfiguration()
-        config.environmentTexturing = .automatic
+        config.planeDetection = [.horizontal]
+        config.environmentTexturing = .none
         arView.session.run(config)
         
         do {
-            let modelEntity = try ModelEntity.load(named: "hhkb_studio")
+            let anchorEntity = AnchorEntity(plane: .any)
+            let modelEntity = try Entity.loadModel(named: "gyoza")
             
-                modelEntity.generateCollisionShapes(recursive: true)
-                
-                let anchorEntity = AnchorEntity(world: [0, 0, -0.5])
-                anchorEntity.addChild(modelEntity)
-                
-                arView.scene.anchors.append(anchorEntity)
-                
+            modelEntity.generateCollisionShapes(recursive: true)
+            anchorEntity.addChild(modelEntity)
+            
+            arView.installGestures(for: modelEntity as Entity & HasCollision)
+            arView.scene.addAnchor(anchorEntity)
         } catch {
             fatalError("3dモデルの読み込みに失敗した: \(error)")
         }
-
-        return arView
         
+        return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+    class Coordinator: NSObject {
+        var view: ARView?
+        
+        @objc
+        func handleLongPress(_ recognizer: UITapGestureRecognizer? = nil) {
+            guard let view = self.view else { return }
+            
+            let tapLocation = recognizer!.location(in: view)
+            
+            if let entity = view.entity(at: tapLocation) as? ModelEntity {
+                if let anchorEntity = entity.anchor {
+                    print("coordinatorが実行された")
+                    anchorEntity.removeFromParent()
+                }
+            }
+        }
+    }
     
 }
 
